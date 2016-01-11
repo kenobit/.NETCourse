ff824236f8e066f203b95fa9ec196d5e6748629b
OstrovskiyMaxim <maxim.ostrovskiy@gmail.com>
2016-01-11 14:15:51 +0200

    Add Paint project to work on

---
+++ Paint/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Paint
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Paint));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btn_color = new System.Windows.Forms.ToolStripButton();
+            this.trk_width = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trk_width)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btn_color});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(513, 44);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btn_color
+            // 
+            this.btn_color.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btn_color.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+            this.btn_color.Image = ((System.Drawing.Image)(resources.GetObject("btn_color.Image")));
+            this.btn_color.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btn_color.Name = "btn_color";
+            this.btn_color.Size = new System.Drawing.Size(87, 41);
+            this.btn_color.Text = "color";
+            this.btn_color.Click += new System.EventHandler(this.btn_color_Click);
+            // 
+            // trk_width
+            // 
+            this.trk_width.Location = new System.Drawing.Point(268, 0);
+            this.trk_width.Margin = new System.Windows.Forms.Padding(7);
+            this.trk_width.Name = "trk_width";
+            this.trk_width.Size = new System.Drawing.Size(245, 56);
+            this.trk_width.TabIndex = 2;
+            this.trk_width.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(278, 375);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 30);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 47);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(489, 322);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(133, 375);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 29);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Load";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Paint
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 406);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.trk_width);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Paint";
+            this.Text = "paint";
+            this.Load += new System.EventHandler(this.Paint_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trk_width)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btn_color;
+        private System.Windows.Forms.TrackBar trk_width;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Paint/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Paint : Form
+    {
+        Bitmap DrawArea;
+        public Paint()
+        {
+            InitializeComponent();
+
+            DrawArea = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
+
+            trk_width.Minimum = 1;
+            trk_width.Maximum = 9;
+            trk_width.TickFrequency = 1;
+
+        }
+
+        int x;
+        int y;
+        bool flag = false;
+        Color penColor = Color.Black;
+        float penWidth = 1;
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            x = e.X;
+            y = e.Y;
+            flag = true;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (flag)
+            {
+
+                Graphics g = Graphics.FromImage(DrawArea);
+                    //pictureBox1.CreateGraphics();
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+                g.DrawLine(new Pen(penColor, penWidth), x, y, e.X, e.Y);
+                x = e.X;
+                y = e.Y;
+                pictureBox1.Image = DrawArea;
+
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            flag = false;
+        }
+
+        private void Paint_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_color_Click(object sender, EventArgs e)
+        {
+            ColorDialog dialogColor = new ColorDialog();
+
+            if (dialogColor.ShowDialog() == DialogResult.OK)
+            {
+                penColor = dialogColor.Color;
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            penWidth = trk_width.Value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Image Files(*.BMP;)|*.BMP|Image Files(*.JPG;)|*.jpg|Image Files(*.GIF;)|*.gif|Image Files(*.PNG;)|*.png|Image Files(*.TIFF;)|*.tiff|Image Files(*.ICON;)|*.ico;";
+
+            if (saveFileDialog1.ShowDialog() != DialogResult.OK) return;
+
+            var savedBit = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Bitmap saveBit = (Bitmap)pictureBox1.Image;
+            
+            pictureBox1.DrawToBitmap(savedBit, pictureBox1.ClientRectangle);
+
+            string path = saveFileDialog1.FileName;
+            if(path != "")
+            {
+                string extention = (path.Substring(path.LastIndexOf('.') + 1)).ToString().ToLower();
+                switch (extention)
+                {
+                    case "jpg":
+                        savedBit.Save(path, System.Drawing.Imaging.ImageFormat.Jpeg);
+                        break;
+                    case "bmp":
+                        savedBit.Save(path, System.Drawing.Imaging.ImageFormat.Bmp);
+                        break;
+                    case "gif":
+                        savedBit.Save(path, System.Drawing.Imaging.ImageFormat.Gif);
+                        break;
+                    case "png":
+                        savedBit.Save(path, System.Drawing.Imaging.ImageFormat.Png);
+                        break;
+                    case "tiff":
+                        savedBit.Save(path, System.Drawing.Imaging.ImageFormat.Tiff);
+                        break;
+                    case "ico":
+                        savedBit.Save(path, System.Drawing.Imaging.ImageFormat.Icon);
+                        break;
+                }
+            }
+
+            savedBit.Dispose();
+            saveFileDialog1.Dispose();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Image Files(*.BMP;)|*.BMP|Image Files(*.JPG;)|*.jpg|Image Files(*.GIF;)|*.gif|Image Files(*.PNG;)|*.png|Image Files(*.TIFF;)|*.tiff|Image Files(*.ICON;)|*.ico;";
+            if (openFileDialog1.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            Bitmap openedBit = new Bitmap(openFileDialog1.FileName);
+
+            Clear_Drawing_area();
+            DrawArea = openedBit;
+            pictureBox1.Image = DrawArea;
+            openFileDialog1.Dispose();
+            openedBit.Dispose();
+        }
+
+        private void Clear_Drawing_area()
+        {
+            DrawArea = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
+            pictureBox1.Image = DrawArea;
+            this.Refresh();
+        }
+    }
+}
+
