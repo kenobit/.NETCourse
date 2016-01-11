ef50dee10f811da616396ed1bf06f78bd67a175d
OstrovskiyMaxim <maxim.ostrovskiy@gmail.com>
2016-01-11 13:30:08 +0200

    First project of Masha's Balls

---
+++ BallMasha/Ball/BBControl.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ball
+{
+    public partial class BBControl : Control
+    {
+        int dx, dy;
+        private Thread thread;
+
+        public BBControl(int X, int Y, int dx, int dy)
+        {
+            InitializeComponent();
+            this.Location = new Point(X, Y);
+            Random rannd = new Random();
+            this.dx = dx;
+            this.dy = dy;
+            while (dy == 0)
+            {
+                this.dy = rannd.Next(-50, 50);
+            }
+            while (dx == 0)
+            {
+                this.dx = rannd.Next(-50, 50);
+            }
+            Color color = Color.FromArgb(rannd.Next(255), rannd.Next(255), rannd.Next(255));
+            while (color == Color.White)
+            {
+                color = Color.FromArgb(rannd.Next(255), rannd.Next(255), rannd.Next(255));
+            }
+            this.BackColor = color;
+            this.Width = 2 * rannd.Next(2, 25);
+            this.Height = this.Width;
+
+            System.Drawing.Drawing2D.GraphicsPath Button_Path = new System.Drawing.Drawing2D.GraphicsPath();
+
+            Button_Path.AddEllipse(0, 0, this.Width, this.Height);
+
+            Region Button_Region = new Region(Button_Path);
+
+            this.Region = Button_Region;
+
+            thread = new Thread(DoWork);
+            thread.Start();
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            Graphics graphics = pe.Graphics;
+            graphics.DrawEllipse(new Pen(this.BackColor), this.Location.X, this.Location.Y, this.Width, this.Height);
+        }
+
+        public new void Move()
+        {
+                int tmpX = Location.X;
+                int tmpY = Location.Y;
+                if (tmpX < 0 || tmpX + Size.Width > Parent.Size.Width)
+                {
+                    dx = -dx;
+                    tmpX = Location.X < 0 ? 0 : Parent.Size.Width - Size.Width;
+                }
+                tmpY = tmpY + dy;
+                if (tmpY < 0 || tmpY + Size.Width + 25 > Parent.Size.Height)
+                {
+                    dy = -dy;
+                    tmpY = tmpY < 0 ? 0 : Parent.Size.Height - Size.Width - 25;
+                }
+                tmpX = tmpX + dx;
+
+                this.Location = new Point(tmpX, tmpY);
+        }
+
+        private void DoWork()
+        {
+            while (true)
+            {
+                Thread.Sleep(25);
+                Invoke(new MethodInvoker(Move));
+            }
+        }
+
+
+    }
+}
---
+++ BallMasha/Ball/BouncingBallForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ball
+{
+    public partial class BouncingBallForm : Form
+    {
+        Thread thread;
+        public BouncingBallForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BouncingBallForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            Random rannd = new Random();
+            int dx = rannd.Next(-25, 25);
+            int dy = rannd.Next(-25, 25);
+            BBControl ball =new BBControl(e.X,e.Y,dx,dy);
+            Controls.Add(ball);
+        }
+    }
+}
